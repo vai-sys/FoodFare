3c67828d69bf05d07c3cc343eb884e0ae419e286
vaishnavi <vaishnavit582@gmail.com>
2024-08-04 17:36:42 +0530

    Recipes

--- Frontend/src/components/Recipes.jsx
+++ Frontend/src/components/Recipes.jsx
@@ -1,5 +1,48 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Card from './Card';
+// import CategoryWrapper from './Category/CategoryWrapper';
+
+// const Recipes = () => {
+//   const [items, setItems] = useState([]);
+
+//   useEffect(() => {
+//     const getLatestItems = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:5000/api/all-items');
+//         setItems(response.data);
+//       } catch (error) {
+//         console.error('Error fetching items:', error);
+//       }
+//     };
+
+//     getLatestItems();
+//   }, []);
+
+//   return (
+//     <div className='px-6 lg:px-12 py-20'>
+//       <h1 className='text-xl text-center font-semibold text-slate-950 sm:text-5xl sm:leading-relaxed'>
+//         All Recipes
+//       </h1>
+//       <CategoryWrapper/>
+//             <ul  className='mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl-grid-cols-4'>
+//              {
+//                items.map((item)=>(
+//                 <Card key={item._id} item={item}/>
+//               ))
+//              }
+//             </ul>
+     
+//     </div>
+//   );
+// };
+
+// export default Recipes;
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from './Card';
+import CategoryWrapper from './Category/CategoryWrapper';
 
 const Recipes = () => {
   const [items, setItems] = useState([]);
@@ -20,19 +63,20 @@ const Recipes = () => {
   return (
     <div className='px-6 lg:px-12 py-20'>
       <h1 className='text-xl text-center font-semibold text-slate-950 sm:text-5xl sm:leading-relaxed'>
-        Recipes
+        All Recipes
       </h1>
-  
-      <div className='mt-8 grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-        {items.map((item) => (
-          <div key={item._id} className='bg-white p-4 rounded-lg shadow'>
-            <h2 className='text-lg font-semibold'>{item.name}</h2>
-            
-          </div>
-        ))}
-      </div>
+      <CategoryWrapper/>
+      <ul className={`mt-20 grid grid-cols-1 ${items.length === 2 ? 'md:grid-cols-2 justify-center' : 'md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'}`}>
+        {
+          items.map((item) => (
+            <Card key={item._id} item={item}/>
+          ))
+        }
+      </ul>
     </div>
   );
 };
 
 export default Recipes;
+
+
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -69,3 +69,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+
