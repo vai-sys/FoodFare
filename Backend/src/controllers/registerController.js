e21fad1c3e1a5a9ae2bef05669002748a23278ba
vaishnavi <vaishnavit582@gmail.com>
2024-08-07 23:53:56 +0530

    Controllers

--- Backend/src/controllers/registerController.js
+++ Backend/src/controllers/registerController.js
@@ -1,59 +1,33 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+const express = require("express");
+const User = require("../model/userModel");
+const bcrypt = require("bcryptjs");
 
-const Register = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setMessage('');
+const register = async (req, res) => {
     try {
-      const response = await axios.post('http://localhost:5000/user/register', {
-        username,
-        password
-      });
-      setMessage(response.data.msg);
-      // Note: The backend doesn't send a token, so we're not storing one
-    } catch (error) {
-      if (error.response) {
-        setMessage(error.response.data.msg || 'An error occurred');
-      } else if (error.request) {
-        setMessage('No response from server. Please try again.');
-      } else {
-        setMessage('Error: ' + error.message);
-      }
-    }
-  };
+        const { username, password } = req.body;
 
-  return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='username'>Username</label>
-          <input 
-            id='username'
-            value={username} 
-            type='text' 
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input 
-            id='password'
-            value={password} 
-            type='password' 
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type='submit'>Register</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
-  );
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const newUser = await User.create({
+            username,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            msg: "User Created Successfully",
+            user: {
+                id: newUser._id,
+                username: newUser.username
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: "Error creating User",
+            error: error.message
+        });
+    }
 };
 
-export default Register;
+module.exports = {
+    register
+};
