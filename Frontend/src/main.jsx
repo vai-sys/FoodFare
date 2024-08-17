8d8552a5092ba570bbc2ff4b638240249bddf508
vaishnavi <vaishnavit582@gmail.com>
2024-08-17 14:21:15 +0530

    Foodfare

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,8 @@
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { AuthProvider } from './components/useAuth.jsx';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import ErrorElement from './components/ErrorElement.jsx';
@@ -17,7 +14,6 @@ import Recipes from './components/Recipes.jsx';
 import RecipePage from './components/RecipePage.jsx';
 import Login from './components/Login.jsx';
 import Register from './components/Register.jsx';
-import ProtectedRoute from '../src/components/ProtectedRoute.jsx';
 
 const router = createBrowserRouter([
   {
@@ -46,39 +42,32 @@ const router = createBrowserRouter([
         element: <Register />
       },
       {
-        element: <ProtectedRoute />,
-        children: [
-          {
-            path: 'recipes',
-            element: <Recipes />
-          },
-          {
-            path: 'categories/:category',
-            element: <CategoryPage />
-          },
-          {
-            path: 'search',
-            element: <Search />
-          },
-          {
-            path: 'recipes/:id',
-            element: <RecipePage />
-          },
-          {
-            path: 'items/:id',
-            element: <SingleProduct />,
-            loader: ({ params }) => fetch(`http://localhost:5000/api/items/${params.id}`)
-          }
-        ]
+        path: 'recipes',
+        element: <Recipes />
+      },
+      {
+        path: 'categories/:category',
+        element: <CategoryPage />
+      },
+      {
+        path: 'search',
+        element: <Search />
+      },
+      {
+        path: 'recipes/:id',
+        element: <RecipePage />
+      },
+      {
+        path: 'items/:id',
+        element: <SingleProduct />,
+        loader: ({ params }) => fetch(`http://localhost:5000/api/items/${params.id}`)
       }
     ]
-  },
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
